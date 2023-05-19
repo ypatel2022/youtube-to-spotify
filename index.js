c6265021c6eb0bad1509e6aabc8615b13e127a90
ypatel2022 <yaxpatel2004@gmail.com>
2023-05-19 16:15:55 -0400

    update program so it takes in data from a json file

---
+++ index.js
@@ -0,0 +1,54 @@
+import ytdl from 'ytdl-core'
+import fs from 'fs'
+
+import { downloadImage, writeTags, convertToMp3, delay } from './utils.js'
+
+import data from './example-songs.json' assert { type: 'json' }
+
+const videoFilePath = './output/video.mp4'
+
+async function main() {
+  for (let i = 0; i < data.length; i++) {
+    const album = data[i]
+
+    const coverFilepath = `./output/${album.album}.jpg`
+
+    await downloadImage(album.cover, coverFilepath).then(() => {
+      console.log(`Downloaded ${album.album} cover`)
+    })
+
+    for (let j = 0; j < album.trackList.length; j++) {
+      const track = album.trackList[j]
+      const url = track.url
+
+      ytdl(url, {
+        filter: 'audioonly',
+        quality: 'highestaudio',
+      }).pipe(fs.createWriteStream(videoFilePath))
+
+      await convertToMp3(videoFilePath).then(() => {
+        console.log(`Converted ${track.title} to mp3`)
+
+        const tags = {
+          title: track.title,
+          artists: track.artists,
+          album: album.album,
+        }
+
+        const finalAudioFilepath = `./output/${album.album}/${track.title}.mp3`
+        const finalAudioDirectory = `./output/${album.album}`
+        if (!fs.existsSync(finalAudioDirectory)) {
+          fs.mkdirSync(finalAudioDirectory)
+        }
+
+        writeTags(tags, coverFilepath, finalAudioFilepath).then(() => {
+          console.log(`Wrote tags to ${track.title}`)
+        })
+      })
+
+      await delay(2000)
+    }
+  }
+}
+
+main()
--- utils.js
+++ utils.js
@@ -1,32 +1,14 @@
-const ytdl = require('ytdl-core')
-const fs = require('fs')
-const ffmpeg = require('fluent-ffmpeg')
-const http = require('http')
-const ID3Writer = require('browser-id3-writer')
+import fs from 'fs'
+import ffmpeg from 'fluent-ffmpeg'
+import http from 'http'
+import ID3Writer from 'browser-id3-writer'
 
 const videoFilepath = './output/video.mp4'
 const audioFilepath = './output/audio.mp3'
-const coverFilepath = './output/cover.jpg'
-
-const url = 'https://www.youtube.com/watch?v=gV2LoFRcebw'
-
-const tags = {
-  title: 'City in the Sky',
-  artists: ['Kanye West'],
-  album: 'Yahndi',
-  TRCK: '0',
-  year: '2016',
-}
 
 const delay = (ms) => new Promise((res) => setTimeout(res, ms))
 
-// get video info
-async function getVideoInfo(url) {
-  const info = await ytdl.getInfo(url)
-  return info
-}
-
-async function downloadImage(imageUrl) {
+async function downloadImage(imageUrl, coverFilepath) {
   // download video thumbnail
   const file = fs.createWriteStream(coverFilepath)
   const request = await http.get(imageUrl, function (response) {
@@ -40,7 +22,7 @@ async function downloadImage(imageUrl) {
   })
 }
 
-async function writeTags() {
+async function writeTags(tags, coverFilepath, finalAudioFilepath) {
   await delay(2500)
 
   const songBuffer = fs.readFileSync(audioFilepath)
@@ -51,7 +33,6 @@ async function writeTags() {
     .setFrame('TIT2', tags.title)
     .setFrame('TPE1', tags.artists)
     .setFrame('TALB', tags.album)
-    .setFrame('TYER', tags?.year)
     .setFrame('APIC', {
       type: 3,
       data: coverBuffer,
@@ -61,7 +42,9 @@ async function writeTags() {
   writer.addTag()
 
   const taggedSongBuffer = Buffer.from(writer.arrayBuffer)
-  fs.writeFileSync(audioFilepath, taggedSongBuffer)
+  fs.writeFileSync(finalAudioFilepath, taggedSongBuffer)
+
+  await delay(1000)
 }
 
 async function convertToMp3() {
@@ -86,30 +69,4 @@ async function convertToMp3() {
   proc.saveToFile(audioFilepath)
 }
 
-// download video mp3
-ytdl(url, {
-  filter: 'audioonly',
-  quality: 'highestaudio',
-}).pipe(fs.createWriteStream(videoFilepath))
-
-convertToMp3().then(() => {
-  console.log('converted to mp3')
-
-  getVideoInfo(url).then((info) => {
-    const thumbnails = info.player_response.videoDetails.thumbnail.thumbnails
-    // replace https with http to download thumbnail
-    const imageUrl = thumbnails[thumbnails.length - 1].url.replace(
-      'https',
-      'http'
-    )
-
-    // make sure thumbnail is downloaded before writing tags
-    downloadImage(imageUrl).then(async () => {
-      await delay(1500)
-
-      writeTags().then(() => {
-        console.log('tags written')
-      })
-    })
-  })
-})
+export { downloadImage, writeTags, convertToMp3, delay }
